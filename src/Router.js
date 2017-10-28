ecb70b9520dfe6de918b139d723230385148db36
Manish Munikar <munikarmanish@gmail.com>
2017-10-28 09:13:56 +0545

    add button goes to new screen

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => (
   <Router>
     <Scene key="root">
-      <Scene key="login" component={LoginForm} title="Please Login" initial />
-      <Scene key="employeeList" type="reset" component={EmployeeList} title="Employees" />
+
+      <Scene
+        key="login"
+        component={LoginForm}
+        title="Please Login"
+        initial
+      />
+
+      <Scene
+        key="employeeList"
+        type="reset"
+        component={EmployeeList}
+        title="Employees"
+        rightTitle="Add"
+        onRight={() => Actions.employeeCreate()}
+      />
+
+      <Scene
+        key="employeeCreate"
+        component={EmployeeCreate}
+        title="Employee"
+      />
     </Scene>
   </Router>
 );
